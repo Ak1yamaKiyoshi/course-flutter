2c3914d8660a91e4badc2418c8b72466c9187f37
akiyama_kiyoshi <108092986+Ak1yamaKiyoshi@users.noreply.github.com>
2023-09-18 13:10:09 +0300

    move due to change repository purpose

--- dart/1_numbers/euclid.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  print(euclid(68, 102));
-} 
-
-int euclid(int a, int b) {
-  if ( b == 0 ) return a;
-  return euclid(b, a % b);
-}
--- dart/1_numbers/even.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  print(isEven(2));
-  print(isEven(1));
-
-}
-
-bool isEven(int i) {
-  return i%2 == 0;
-}
